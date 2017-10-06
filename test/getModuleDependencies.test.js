6fd8397f93115f572727509dd61e037be325fa18
Ferdinand Prantl <prantlf@gmail.com>
2017-10-06 13:18:56 +0200

    Look for symbol location only in direct module dependencies by modified amodro-trace/parse

--- test/getModuleDependencies.test.js
+++ test/getModuleDependencies.test.js
@@ -2,50 +2,62 @@ const assert = require('assert');
 const { ReferenceProvider } = require('../extension');
 const referenceProvider = new ReferenceProvider();
 
-suite('getModulesWithPathFromRequireOrDefine', () => {
+suite('getModuleDependencies', () => {
 	test('should return object with module path and name', () => {
-		const input = 'define([\'./path/to/a\', \'./path/to/b\'], function (moduleA, moduleB) {';
+		const input = 'define([\'./path/to/a\', \'./path/to/b\'], function (moduleA, moduleB) {});';
 		const expected = {
 			moduleA: './path/to/a',
 			moduleB: './path/to/b'
 		};
 
-		assert.deepEqual(referenceProvider.getModulesWithPathFromRequireOrDefine(input), expected);
+		assert.deepEqual(referenceProvider.getModuleDependencies({
+			fileName: '1',
+			version: 1
+		}, input), expected);
 	});
 
 	test('should return object with module path and name for multiline define', () => {
 		const input = `define([
                 'moduleA', 
                 'moduleB'
-            ], function(a, b) {`;
+            ], function(a, b) {});`;
 		const expected = {
 			a: 'moduleA',
 			b: 'moduleB'
 		};
 
-		assert.deepEqual(referenceProvider.getModulesWithPathFromRequireOrDefine(input), expected);
+		assert.deepEqual(referenceProvider.getModuleDependencies({
+			fileName: '2',
+			version: 1
+		}, input), expected);
 	});
 
 	test('should return object with module path and name for multiline define', () => {
 		const input = `require([
                 'moduleA', 
                 'moduleB'
-            ], function(a, b) {`;
+            ], function(a, b) {});`;
 		const expected = {
 			a: 'moduleA',
 			b: 'moduleB'
 		};
 
-		assert.deepEqual(referenceProvider.getModulesWithPathFromRequireOrDefine(input), expected);
+		assert.deepEqual(referenceProvider.getModuleDependencies({
+			fileName: '3',
+			version: 1
+		}, input), expected);
 	});
 
 	test('should return object with module path and name for named module', () => {
-		const input = 'define(\'myName\', [\'moduleA\', \'moduleB\'], function(a, b) {';
+		const input = 'define(\'myName\', [\'moduleA\', \'moduleB\'], function(a, b) {});';
 		const expected = {
 			a: 'moduleA',
 			b: 'moduleB'
 		};
 
-		assert.deepEqual(referenceProvider.getModulesWithPathFromRequireOrDefine(input), expected);
+		assert.deepEqual(referenceProvider.getModuleDependencies({
+			fileName: '4',
+			version: 1
+		}, input), expected);
 	});
 });
